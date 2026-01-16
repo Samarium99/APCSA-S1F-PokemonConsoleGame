f4b5dfc97ae416546ba562460eac5339de82b83a
Samarium99 <sarah.mo@csedge.org>
2026-01-16 13:19:35 -0500

    Add Move.java

---
+++ Move.java
@@ -0,0 +1,14 @@
+public class Move() {
+    int pow; // power
+    int acc; // accuracy
+
+    public Move(pow, acc){
+        this.pow = pow;
+        this.acc = acc;
+    }
+
+    public void setpow(){
+
+    }
+
+}
