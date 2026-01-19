f7e1296ed769084fade40a63d3f36356d3ced229
Samarium99 <sarah.mo@csedge.org>
2026-01-19 01:25:37 -0500

    Update Driver.java Pokemon

--- Driver.java
+++ Driver.java
@@ -1,5 +1,10 @@
 public class Driver{
     public static void main(String[] args){
-        Pokemon Togepi = new Player("");
+        Pokemon Togepi = new Player("Togepi", );
+        Pokemon Pichu = new Player("Pichu", );
+        Pokemon Cyndaquil = new Player("Cyndaquil", );
+
+        Pokemon Porygon = new Gym("Porygon", );
+        Pokemon Togepi = new Player("Togepi", );
     }
 }
