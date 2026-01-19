d666668bb87c7d713417ed65b918952ec0b01f06
Samarium99 <sarah.mo@csedge.org>
2026-01-19 01:31:38 -0500

    Update Move getters

--- Move.java
+++ Move.java
@@ -1,12 +1,25 @@
 public class Move() {
+    String name;
     int pow; // power
     int acc; // accuracy
 
-    public Move(pow, acc){
+    public Move(name, pow, acc){
+        this.name = 
         this.pow = pow;
         this.acc = acc;
     }
+    // getters
+    public String getName(){
+        return name;
+    }
+    public int getPow() {
+        return pow;
+    }
+    public int getAcc(){
+        return acc;
+    }
 
+    // setters
     public void powChange(multipler) { // increases/decreases power by multiplier
         this.pow = pow * multipler;
     }
