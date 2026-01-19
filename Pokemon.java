837315800cbdcb929a013ada4dae3964dfffd776
Samarium99 <sarah.mo@csedge.org>
2026-01-19 01:33:29 -0500

    Update Pokemon.java Constructors and Instance vars

--- Pokemon.java
+++ Pokemon.java
@@ -4,10 +4,16 @@ public abstract class Pokemon{
   private String name;
   private int HP,maxHP;
   private int def;
+  private Move move1, move2, move3, move4;
 
-  public abstract Pokemon();
-  public abstract Pokemon(String name);
-  public abstract Pokemon(String name, int hp);
+  // empty constructor unused, because parameters unused
+  public Pokemon(){
+    name = "Olikoali";
+    HP, maxHP = 1000;
+    def = 100;
+    move1, move2, move3, move4 = new Move ("Sleep", 0, 100);
+  }
+  public abstract Pokemon(String name, int maxHP, int def, Move move1, Move move2, Move move3, Move move4);
 
   public String getName(){
     return name;
