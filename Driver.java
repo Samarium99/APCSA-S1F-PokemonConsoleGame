48785bb2f1e207c502cc1a1b46e63ac5e12d6c03
Samarium99 <sarah.mo@csedge.org>
2026-01-19 23:36:02 -0500

    Continue, WIP: Round System

--- Driver.java
+++ Driver.java
@@ -1,38 +1,55 @@
 import java.util.Scanner;
 public class Driver{
+    static int numRounds = 0;
+    static String userName = "New Kid";
+    
+    static Pokemon[] playerTeam;
+    static Trainer player;
+
+    static Pokemon[] gymTeam;
+    static Trainer gymLeader;
+
     public static void main(String[] args){
-        int numRounds = 0;
         setUp();   
 
         Scanner sc = new Scanner(System.in); 
-        String userName = introDialogue(sc);
+        introDialogue(sc);
         
-        Trainer player = new Player(userName, );
-        Trainer gymLeader = new GymLeader();
-        while ()
+        player = new Player(userName, playerTeam);
+        gymLeader = new GymLeader("Gym Leader Olikoali", gymTeam);
 
+        while (!player.lost() && !gymLeader.lost() && numRounds <= 25){
+            System.out.println("-----\nROUND "+intToString(numRounds));
+            playRound(sc);
+            numRounds++;
+        }
+        if (numRounds > 25){
+            System.out.print("\u001b[3m(You run out of patience for this game. You and Gym Leader OliKoali shake hands and decide to resolve tensions amicably.)\n> Great!");
+        } else {
+            printResults();
+        }
     }
     
     public static void setUp(){ // Code for initializing game details
-        // Move tackle = new Move("Tackle", 30, 90);
-        // Move quickAttack = new Move("Quick Attack", 40, 80);
-        // Move tailWhip = new Move("Tail Whip", 0, 100, 0.5);
-        // Move thunderShock = new Move("Thunder Shock", 40, 80);
+        Move tackle = new Move("Tackle", 30, 90);
+        Move quickAttack = new Move("Quick Attack", 40, 80);
+        Move tailWhip = new Move("Tail Whip", 0, 100, 0.5);
+        Move thunderShock = new Move("Thunder Shock", 40, 80);
 
-        // Pokemon togepi = new Pokemon("Togepi", 200, 60, tackle, new Move("Fairy Wind", 40, 80), new Move("Draining Kiss", 40, 60, 30), new Move("Peck", 35, 100));
-        // Pokemon pichu = new Pokemon("Pichu", 225, 40, new Move("Nuzzle", 30, 100), thunderShock, quickAttack, tailWhip);
-        // Pokemon cyndaquil = new Pokemon("Cyndaquil", 250, 70, quickAttack, new Move("Flame Wheel", 60, 60), new Move("Inferno", 100, 45), tailWhip);
-        // Pokemon[] playerTeam = {togepi, pichu, cyndaquil};
+        Pokemon togepi = new Pokemon("Togepi", 200, 60, tackle, new Move("Fairy Wind", 40, 80), new Move("Draining Kiss", 40, 60, 30), new Move("Peck", 35, 100));
+        Pokemon pichu = new Pokemon("Pichu", 225, 40, new Move("Nuzzle", 30, 100), thunderShock, quickAttack, tailWhip);
+        Pokemon cyndaquil = new Pokemon("Cyndaquil", 250, 70, quickAttack, new Move("Flame Wheel", 60, 60), new Move("Inferno", 100, 45), tailWhip);
+        playerTeam = new Pokemon[]{togepi, pichu, cyndaquil, null, null, null};
 
-        // Pokemon porygon = new Pokemon("Porygon", 230, 70, tackle, thunderShock, new Move("Recover", 0, 30, 100), new Move("Headbutt", 30, 90));
-        // Pokemon arbok = new Pokemon("Arbok", 200, 50, tailWhip, new Move("Bite", 40, 80), new Move("Leech Life", 30, 50, 25), tackle);
-        // Pokemon[] gymTeam = {arbok, porygon};
+        Pokemon porygon = new Pokemon("Porygon", 230, 70, tackle, thunderShock, new Move("Recover", 0, 30, 100), new Move("Headbutt", 30, 90));
+        Pokemon arbok = new Pokemon("Arbok", 200, 50, tailWhip, new Move("Bite", 40, 80), new Move("Leech Life", 30, 50, 25), tackle);
+        gymTeam = new Pokemon[]{arbok, porygon, null, null, null, null};
     }
-    public static String introDialogue(Scanner input){
+    public static void introDialogue(Scanner input){
         System.out.print("\u001b[33m");
         System.out.println("\n** HOW TO PLAY: To advance dialogue, press the ENTER key **");
         System.out.print("Please answer with what you would like to be called:\n> ");
-        String userName = input.nextLine();
+        userName = input.nextLine();
         System.out.println("---Starting Game...");
         System.out.print("\u001b[0m\u001b[1mYoungster Samarium99:\t");
         System.out.println("\u001b[0m"+userName+"! There you are! Professor Novillo needed to ask you a favor, but she is busy preparing another exam for her 7th period APCSA class, so I'm here to carry along the message.");
@@ -58,12 +75,35 @@ public class Driver{
         input.nextLine();
         System.out.println("\u001b[31mGym Leader OliKoali has challenged you to a Pokemon Battle!");
         input.nextLine();
-        return userName;
+    }
+    public static void printResults() {
+        System.out.print("The game ended in ");
+        if (player.lost() == gymLeader.lost()){
+            System.out.print("a DRAW.");
+        } else if (player.lost()){
+            System.out.print("DEFEAT :(");
+        } else if (gymLeader.lost()){
+            System.out.print("VICTORY! :D");
+        }
     }
 
-    public static void playRound(){
+    public static String intToString(int num){
+        if (num >= 20) {
+            return num == 20 ? "TWENTY" : "TWENTY-"+intToString(num-20);
+        } else if (num > 10 && num < 20) {
+            String[] teens = {"ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN"};
+            return teens[num-11];
+        } else if (num > 0 && num <= 10) {
+            String[] ones = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN"};
+            return ones[num-1];
+        }
+        return "ZERO";
+    }
+
+    public static void playRound(Scanner input){
         System.out.println("");
         
     }
+
 }
 // The best Johto Starter is Chikorita!
